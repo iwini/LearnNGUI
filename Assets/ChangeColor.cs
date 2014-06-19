9ade27a519c1cd7d42e7a1b4c8dad1fc35ed3b02
iwini <iwini@vip.qq.com>
2014-06-19 11:22:11 +0800

    Add＋Learn 3

---
+++ Assets/ChangeColor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeColor : MonoBehaviour {
+	public void changeToRed(){
+		GetComponent<UIWidget>().color = Color.red;
+	}
+
+	public void changeToGreen(){
+		GetComponent<UIWidget>().color = Color.green;
+	}
+
+	public void changeToBlue(){
+		GetComponent<UIWidget>().color = Color.blue;
+	}
+}
